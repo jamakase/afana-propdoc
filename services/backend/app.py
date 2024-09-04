63f89c1ea911ca8e2c357446313d113a4aeb6e04
Анна Тищенко <tischenko.anna.a@yandex.ru>
2024-09-05 00:31:20 +0300

    update app.py, refactor

--- services/backend/app.py
+++ services/backend/app.py
@@ -1,7 +1,36 @@
 from flask import Flask
+from config import Config
+from celery_init import celery_init_app
+from models import db
+from routes.message.send_message import send_message
+from routes.task.check_task_result import task_result
+from routes.conversation.create_conversation import  create_conversation
 
 
 def create_app():
     app = Flask(__name__)
 
-    return app
+
+    with app.app_context():
+        app.config.from_object(Config)
+
+        db.init_app(app)
+
+        app.config.from_mapping(
+            CELERY=dict(
+                broker_url="redis://localhost",
+                result_backend="redis://localhost",
+                task_ignore_result=True,
+            ),
+        )
+
+        app.config.from_prefixed_env()
+        celery = celery_init_app(app)
+
+        db.create_all()
+
+    app.add_url_rule('/message', view_func=send_message, methods=['POST'])
+    app.add_url_rule('/task/<id>', view_func=task_result, methods=['GET'])
+    app.add_url_rule('/conversation/create', view_func=create_conversation, methods=['POST'])
+
+    return app, celery
--- services/backend/celery_worker.py
+++ services/backend/celery_worker.py
@@ -1,4 +1,4 @@
-from app import create_app
+from services.backend.app import create_app
 
 app, celery = create_app()
 app.app_context().push()
