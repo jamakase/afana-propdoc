d02820d833840bb47739717c1c01b4372b7bce6e
Анна Тищенко <tischenko.anna.a@yandex.ru>
2024-09-05 00:03:25 +0300

    add models

---
+++ services/backend/models/message_model.py
@@ -0,0 +1,19 @@
+from models import db
+
+
+class MessageModel(db.Model):
+    __tablename__ = 'messages'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    conversation_id = db.Column(db.Integer, db.ForeignKey('conversations.id'), nullable=False)
+    task_id = db.Column(db.String, nullable=True)
+
+    role = db.Column(db.Integer, nullable=False)  # 1 - 'system' или 2 - 'user'
+    text = db.Column(db.Text, nullable=False)
+
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f"<Message(id={self.id}, conversation_id={self.conversation_id} role={self.role}, text={self.text})>"
