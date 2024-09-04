041ce1547710a71ceecdcbee48c4d1619e24b809
Анна Тищенко <tischenko.anna.a@yandex.ru>
2024-09-05 00:15:40 +0300

    add config.py

---
+++ services/backend/config.py
@@ -0,0 +1,5 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///api_backend.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CELERY_BROKER_URL = 'redis://localhost:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
