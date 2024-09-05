61a52460142250f45a29af793cf879665d638abd
Анна Тищенко <tischenko.anna.a@yandex.ru>
2024-09-05 18:45:23 +0300

    add UUID

--- services/backend/models/conversation_model.py
+++ services/backend/models/conversation_model.py
@@ -1,11 +1,12 @@
 from services.backend.models import db
-
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
 
 class ConversationModel(db.Model):
     __tablename__ = 'conversations'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=True)
+    user_id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=True)
 
     def __repr__(self):
         return f"<Conversation(id={self.id}, user_id={self.user_id})>"
