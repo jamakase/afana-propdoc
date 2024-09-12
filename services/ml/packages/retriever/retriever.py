28f5fcab39183b21382973107a28edc7d85de0cf
Artem Astapenko <jamakase54@gmail.com>
2024-09-12 15:00:43 +0300

    Fix issue with qdrant key

--- services/ml/packages/retriever/retriever.py
+++ services/ml/packages/retriever/retriever.py
@@ -2,10 +2,13 @@ from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
 from langchain_core.embeddings import Embeddings
 
+
 class Retriever:
-    def __init__(self, embeddings: Embeddings, host: str, collection_name: str):
-        client = QdrantClient(url=host)
-        
+    def __init__(
+        self, embeddings: Embeddings, host: str, collection_name: str, api_key: str
+    ):
+        client = QdrantClient(url=host, api_key=api_key)
+
         # Check if collection exists, if not, create it
         collections = client.get_collections().collections
         if not any(collection.name == collection_name for collection in collections):
@@ -14,7 +17,7 @@ class Retriever:
                 vectors_config={
                     "size": 384,
                     "distance": "Cosine",
-                }
+                },
             )
 
         self.vectorstore = QdrantVectorStore(
