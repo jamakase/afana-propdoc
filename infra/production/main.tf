45e6831298512889e05be903410dde4c172edf4a
Artem Astapenko <jamakase54@gmail.com>
2024-09-13 11:41:14 +0300

    Commit removed info

--- infra/production/main.tf
+++ infra/production/main.tf
@@ -35,7 +35,7 @@ module "app" {
     CELERY_RESULT_BACKEND   = "redis://:password@redis:6379/0"
     OPENROUTER_API_KEY = "a"
     QDRANT_API_KEY = var.qdrant_api_key
-    QDRANT_COLLECTION_NAME = "new_docs"
+    QDRANT_COLLECTION_NAME = "docs_768"
     OPENAI_API_KEY = var.openai_api_key
     YC_API_KEY = yandex_iam_service_account_api_key.instance_service_account_api_key.secret_key
     FOLDER_ID = var.folder_id
